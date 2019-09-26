771ce989a9d63e2cd5d83050df8037178182903e
Marc <marc.g.smith@gmail.com>
2019-09-26 21:12:53 +0100

    Login request entity

---
+++ src/main/java/uk/co/stuffusell/api/common/LoginRequest.java
@@ -0,0 +1,25 @@
+package uk.co.stuffusell.api.common;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE, fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class LoginRequest {
+    private String email;
+    private String password;
+
+    private LoginRequest() {
+
+    }
+
+    public LoginRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+}
